7585f984b71e44f0b5a2cf88c718c7c0da117993
nashihu <ahmad.n.id@gmail.com>
2023-01-03 21:24:12 +0700

    gtw

--- src/shared/App.js
+++ src/shared/App.js
@@ -17,6 +17,10 @@ export default class App extends Component {
             <div>
                 <Helmet>
                     <title>App Component | React Universal</title>
+                    <meta property="og:title" content="Yayasan Berkah Yatim Indonesia" />
+                    <meta property="og:description" content="Membangun Generasi Qurani, Berkarakter Dan Visioner" />
+                    <meta property="og:image"
+                        content="https://raw.githubusercontent.com/nashihu/production_stuff/master/bsy_images/2020-01-24%2018.19.45.jpeg" />
                 </Helmet>
 
                 <h1>Welcome to React Fiber.</h1>
